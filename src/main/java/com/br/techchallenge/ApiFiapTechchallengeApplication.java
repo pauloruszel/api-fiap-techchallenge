44c1848621b7a2db90803d4013132a0820496d3e
Paulo Ruszel <psantos@iblueconsulting.com.br>
2024-04-29 10:49:11 -0300

    Comite inicial

---
+++ src/main/java/com/br/techchallenge/ApiFiapTechchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.br.techchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFiapTechchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFiapTechchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/techchallenge/ApiFiapTechchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.techchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFiapTechchallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
